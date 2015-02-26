029b78dfe8f0497519d304401cece22eb718c243
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2015-02-26 16:26:18 -0500

    Translating the registration page

--- action/RegisterOverride.php
+++ action/RegisterOverride.php
@@ -36,10 +36,11 @@ class action_plugin_door43register_RegisterOverride extends DokuWiki_Action_Plug
         $event->stopPropagation();
         $event->preventDefault();
 
-        self::override_html_register();
+
+        $this->override_html_register();
     }
 
-    static function override_html_register(){
+    protected function override_html_register(){
         global $lang;
         global $conf;
         global $INPUT;
@@ -47,7 +48,7 @@ class action_plugin_door43register_RegisterOverride extends DokuWiki_Action_Plug
         $base_attrs = array('size'=>50,'required'=>'required');
         $email_attrs = $base_attrs + array('type'=>'email','class'=>'edit');
 
-        print self::override_locale_xhtml('register');
+        print $this->override_locale_xhtml('register');
         print '<div class="centeralign">'.NL;
         $form = new Doku_Form(array('id' => 'dw__register'));
         $form->startFieldset($lang['btn_register']);
@@ -67,18 +68,81 @@ class action_plugin_door43register_RegisterOverride extends DokuWiki_Action_Plug
         print '</div>'.NL;
     }
 
-    static function override_locale_xhtml($id){
+    protected function override_locale_xhtml($id){
         //fetch parsed locale
-        $html = p_cached_output(self::override_localeFN($id));
+        $html = $this->override_cached_output($this->override_localeFN($id));
         return $html;
     }
 
-    static function override_localeFN($id,$ext='txt'){
+    protected function override_localeFN($id, $ext='txt'){
         global $conf;
         $file = DOKU_CONF.'lang/'.$conf['lang'].'/'.$id.'.'.$ext;
         if(!@file_exists($file)){
             $file = DOKU_CONF.'lang/en/'.$id.'.'.$ext;
         }
         return $file;
+    }
+
+    protected function override_cached_output($file, $format='xhtml', $id='') {
+        global $conf;
+
+        $cache = new cache_renderer($id, $file, $format);
+        if ($cache->useCache()) {
+            $parsed = $cache->retrieveCache(false);
+            if($conf['allowdebug'] && $format=='xhtml') $parsed .= "\n<!-- cachefile {$cache->cache} used -->\n";
+        } else {
+            $parsed = p_render($format, p_cached_instructions($file,false,$id), $info);
+
+            // if there is a user logged in, insert the translate button
+            if (($GLOBALS['USERINFO'] != null) && (!empty($GLOBALS['USERINFO']['grps'])))
+                $parsed .= PHP_EOL . $this->getButton();
+
+            if ($info['cache'] && $cache->storeCache($parsed)) {              // storeCache() attempts to save cachefile
+                if($conf['allowdebug'] && $format=='xhtml') $parsed .= "\n<!-- no cachefile used, but created {$cache->cache} -->\n";
+            }else{
+                $cache->removeCache();                     //try to delete cachefile
+                if($conf['allowdebug'] && $format=='xhtml') $parsed .= "\n<!-- no cachefile used, caching forbidden -->\n";
+            }
+        }
+
+        return $parsed;
+    }
+
+    protected function getButton() {
+
+        $buttonText = file_get_contents(dirname(dirname(__FILE__)) . '/private/html/translate_button.html');
+
+        // remove suppress comments
+        $buttonText = preg_replace('/\<!--(\s)*suppress(.)*--\>(\n)/', '', $buttonText, 1);
+
+        // remove the initial doc comments
+        $buttonText = preg_replace('/^\<!--(.|\n)*--\>(\n)/', '', $buttonText, 1);
+
+        //<!--suppress
+        $buttonText = $this->translateHtml($buttonText);
+
+        /* @var $translation helper_plugin_translation */
+        $translation = plugin_load('helper','translation');
+        $langName = $translation->getLocalName($GLOBALS['conf']['lang']);
+        if (empty($langName)) $langName = $GLOBALS['conf']['lang'];
+
+        // insert the name of the current language and language code
+        $buttonText = str_replace('{lang}', $langName, $buttonText);
+        $buttonText = str_replace('{langCode}', $GLOBALS['conf']['lang'], $buttonText);
+
+        // put the text to translate into the edit area
+        $fileText = file_get_contents($this->override_localeFN('register'));
+        $buttonText = str_replace('</textarea>', $fileText . '</textarea>', $buttonText);
+
+        return $buttonText;
+    }
+
+    protected function translateHtml($html) {
+        return preg_replace_callback('/@(.+?)@/',
+            function($matches) {
+                $text = $this->getLang($matches[1]);
+                return (empty($text)) ? $matches[0] : $text;
+            },
+            $html);
     }
 }
